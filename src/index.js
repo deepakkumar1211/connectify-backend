5e338685d63483db7eb7fced0c266e399b5d4395
Raj <rajdewangan020@gmail.com>
2024-10-17 19:55:03 +0530

    minor changes by raj

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@
 // })
 
 
-// // Database connection
-// connectDB()
-//     .then(() => {
-//         app.listen(process.env.PORT || 8000, () => {
-//             console.log(`server is running at port : ${process.env.PORT}`);
-//         })
-//     })
-//     .catch((err) => {
-//         console.log("Mongo DB connection failed !!!", err);
+// Database connection
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Mongo DB connection failed !!!", err);
 
-//     })
+    })
 
 
 // app.get("/", (req, res) => {
@@ -27,21 +27,4 @@
 // })
 
 
-import dotenv from "dotenv";
-import { app } from "./app.js";
-import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './.env'
-});
-
-// Database connection
-export default async (req, res) => {
-    try {
-        await connectDB(); // Ensure the DB is connected for each request
-        return app(req, res); // Delegate the request to your Express app
-    } catch (err) {
-        console.error("MongoDB connection failed!", err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-};
