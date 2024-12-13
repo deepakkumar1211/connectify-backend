599cd6f5169cc45ccc7ec913d57aec1bc1bc3aad
deepakkumar1211 <sahudeepak1211.com>
2024-12-13 14:27:01 +0530

    changes the multer file path

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -20,6 +20,7 @@ const postSchema = new Schema(
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User",
+            // required: true
         }
     },
     {timestamps: true
